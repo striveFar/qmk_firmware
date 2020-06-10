3c48f6c3ece470a04e902c590404f26570acf046
Victor L <lucachi.victor@gmail.com>
2020-06-10 17:17:05 +0300

    oops fixed small typo

--- keyboards/handwired/void40/keymaps/353/keymap.c
+++ keyboards/handwired/void40/keymaps/353/keymap.c
@@ -57,10 +57,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_void_grid(
-    KC_TILD,  KC_EXLM,    KC_AT,    KC_HASH,  KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,     KC_ASTR,    KC_LPRN, KC_RPRN, KC_TRANS,
+    KC_TILD,  KC_EXLM,    KC_AT,    KC_HASH,  KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,     KC_ASTR,    KC_LPRN, KC_RPRN, KC_TRNS,
     KC_DEL,   KC_F1,      KC_F2,    KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_UNDS,     KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
     KC_CAPS,  KC_F7,      KC_F8,    KC_F9,    KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS),  S(KC_NUBS), KC_HOME, KC_END,  KC_ENT,
-    KC_ESC,   TG(_CSGO),  KC_TRANS, KC_TRANS, KC_TRNS, KC_MPLY, KC_MPLY, KC_TRNS,     KC_MPRV,    KC_VOLD, KC_VOLU, KC_MNXT
+    KC_ESC,   TG(_CSGO),  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_MPLY, KC_MPLY, KC_TRNS,     KC_MPRV,    KC_VOLD, KC_VOLU, KC_MNXT
 ),
 
 /* Raise
@@ -75,10 +75,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_void_grid(
-    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_TRANS,
+    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
     KC_DEL,   KC_F1,    KC_F2,    KC_F3,    KC_F4,   KC_F5,   KC_F6,    KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    KC_CAPS,  KC_F7,    KC_F8,    KC_F9,    KC_F10,  KC_F11,  KC_F12,   KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, KC_TRANS,
-    KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRNS, KC_MPLY, KC_MPLY,  KC_TRNS, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
+    KC_CAPS,  KC_F7,    KC_F8,    KC_F9,    KC_F10,  KC_F11,  KC_F12,   KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_MPLY, KC_MPLY,  KC_TRNS, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
 ),
 
 /* CSGO */
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_NO, KC_TRANS,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+    KC_NO, KC_TRNS,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
 ),
 
 /* Adjust */
