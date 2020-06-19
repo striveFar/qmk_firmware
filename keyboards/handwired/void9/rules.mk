0f31af94c23d6c94bec32a8d80f861a1395368a0
Victor L <lucachi.victor@gmail.com>
2020-06-19 12:45:17 +0300

    disabled tapdance/enabled mouse and extra keys

--- keyboards/handwired/void9/rules.mk
+++ keyboards/handwired/void9/rules.mk
@@ -31,4 +31,4 @@ AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
 
-TAP_DANCE_ENABLE=yes		# Enable Tap Dance
+TAP_DANCE_ENABLE=no		# Enable Tap Dance
