f6d0144ba042ded4c52d4d28102cd08d453771e4
Victor Lucachi <lucachi.victor@gmail.com>
2020-09-23 17:26:02 +0300

    formatting

--- keyboards/handwired/void40/config.h
+++ keyboards/handwired/void40/config.h
@@ -20,12 +20,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID 0xF055    // VOID
-#define PRODUCT_ID 0x3531   // VOID40
+#define VENDOR_ID 0xF055
+#define PRODUCT_ID 0x3531
 #define DEVICE_VER 0x0001
-#define MANUFACTURER        Victor Lucachi
-#define PRODUCT             VOID40
-#define DESCRIPTION         A handwired 40% 12x4 ortholinear keyboard
+#define MANUFACTURER Victor Lucachi
+#define PRODUCT VOID40
+#define DESCRIPTION A handwired 40% 12x4 ortholinear keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 4
