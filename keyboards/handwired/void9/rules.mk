06d56beb324f70395aee37da1a23e7acdf40f9a5
Victor Lucachi <lucachi.victor@gmail.com>
2020-09-23 17:48:07 +0300

    formatting

--- keyboards/handwired/void9/rules.mk
+++ keyboards/handwired/void9/rules.mk
@@ -14,11 +14,11 @@ BOOTLOADER = caterina
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = lite       # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no       # Mouse keys
-EXTRAKEY_ENABLE = no       # Audio control and System control
+BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no       	# Mouse keys
+EXTRAKEY_ENABLE = no       	# Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
-COMMAND_ENABLE = no        # Commands for debug and configuration
+COMMAND_ENABLE = no        	# Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
@@ -29,6 +29,5 @@ MIDI_ENABLE = no            # MIDI support
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
 
-TAP_DANCE_ENABLE=no		# Enable Tap Dance
+TAP_DANCE_ENABLE=no			# Enable Tap Dance
