89bf5fbf0dd15f2a4289669b788d2187b6e961f0
fairglen <dias.lopes.leo@gmail.com>
2021-03-04 19:12:55 +0000

    :sparkles: Change space cadet mod key to LGUI

--- keyboards/handwired/void_ergo/keymaps/leo/keymap.c
+++ keyboards/handwired/void_ergo/keymaps/leo/keymap.c
@@ -38,10 +38,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                       |-------------------------|  |---------------------------|
  */
 [_QWERTY] = LAYOUT_void_ergo(
-    KC_ESC,     KC_Q,       KC_W,       KC_E,       KC_R,           KC_T,                    KC_Y,      KC_U,       KC_I,       KC_O,       KC_P,       KC_BSPC,
-    KC_LSFT,    KC_A,       KC_S,       KC_D,       KC_F,           KC_G,                    KC_H,      KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
-    KC_LCTL,    KC_Z,       KC_X,       KC_C,       KC_V,           KC_B,                    KC_N,      KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_ENT,
-                LGUI(KC_C), MO(_LOWER),  KC_LGUI,   LCAG_T(KC_SPC), KC_LAPO,                 KC_RAPC,   KC_SPC,     KC_RGUI,    MO(_RAISE), LGUI(KC_V)
+    KC_ESC,     KC_Q,       KC_W,       KC_E,       KC_R,           KC_T,                    KC_Y,      KC_U,   KC_I,                                       KC_O,       KC_P,       KC_BSPC,
+    KC_LSFT,    KC_A,       KC_S,       KC_D,       KC_F,           KC_G,                    KC_H,      KC_J,   KC_K,                                       KC_L,       KC_SCLN,    KC_QUOT,
+    KC_LCTL,    KC_Z,       KC_X,       KC_C,       KC_V,           KC_B,                    KC_N,      KC_M,   KC_COMM,                                    KC_DOT,     KC_SLSH,    KC_ENT,
+                LGUI(KC_C), MO(_LOWER), KC_LGUI,    KC_SPC,         KC_LAPO,                 KC_RAPC,   KC_SPC, LM(_LOWER, MOD_LCTL | MOD_LALT | MOD_LGUI), MO(_RAISE), LGUI(KC_V)
 ),
 /* _LOWER
  * |-----------------------------------------------------.                    .-----------------------------------------------------|
