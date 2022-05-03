5b684fed535b027093be0ea9b7d9d708e8457e1d
JX <572673807@qq.com>
2022-05-04 03:18:23 +0800

    init nrf52 port

---
+++ keyboards/yandrstudio/nrf/ninekeytest/rules.mk
@@ -0,0 +1,13 @@
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes        # Mouse keys
+COMMAND_ENABLE = yes         # Commands for debug and configuration
+CONSOLE_ENABLE = no         # Console for debug
+EXTRAKEY_ENABLE = yes        # Audio control and System control
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+OLED_DRIVER = SSD1306
+OLED_ENABLE = yes
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
---
+++ platforms/nrf52/pin_defs.h
@@ -0,0 +1,63 @@
+/* Copyright 2022 JasonRen(biu)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#pragma once
+
+#define GPIO(port, pin) ((port << 5) | pin)
+
+// orion87 rows & cols
+#define P0 GPIO(0,0)
+#define P1 GPIO(0,1)
+#define P2 GPIO(0,2)
+#define P3 GPIO(0,3)
+#define P4 GPIO(0,4)
+#define P5 GPIO(0,5)
+#define P6 GPIO(0,6)
+#define P7 GPIO(0,7)
+#define P8 GPIO(0,8)
+#define P9 GPIO(0,9)
+#define P10 GPIO(0,10)
+#define P11 GPIO(0,11)
+#define P12 GPIO(0,12)
+#define P13 GPIO(0,13)
+#define P14 GPIO(0,14)
+#define P15 GPIO(0,15)
+#define P16 GPIO(0,16)
+#define P17 GPIO(0,17)
+#define P18 GPIO(0,18)
+#define P19 GPIO(0,19)
+#define P20 GPIO(0,20)
+#define P21 GPIO(0,21)
+#define P22 GPIO(0,22)
+#define P23 GPIO(0,23)
+#define P24 GPIO(0,24)
+#define P25 GPIO(0,25)
+#define P26 GPIO(0,26)
+#define P27 GPIO(0,27)
+#define P28 GPIO(0,28)
+#define P29 GPIO(0,29)
+#define P30 GPIO(0,30)
+#define P31 GPIO(0,31)
+#define PP0 GPIO(1,0)
+#define PP1 GPIO(1,1)
+#define PP2 GPIO(1,2)
+#define PP3 GPIO(1,3)
+#define PP4 GPIO(1,4)
+#define PP5 GPIO(1,5)
+#define PP6 GPIO(1,6)
+#define PP7 GPIO(1,7)
+
