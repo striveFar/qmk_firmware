7e2f6f3c243bc77cdb3e8aa53964b74c5b0284e3
JX <572673807@qq.com>
2022-05-30 01:22:25 +0800

    Update rules.mk

--- keyboards/yandrstudio/nrf/ninekeytest/rules.mk
+++ keyboards/yandrstudio/nrf/ninekeytest/rules.mk
@@ -2,12 +2,12 @@
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes        # Mouse keys
-COMMAND_ENABLE = yes         # Commands for debug and configuration
-CONSOLE_ENABLE = no         # Console for debug
-EXTRAKEY_ENABLE = yes        # Audio control and System control
-NKRO_ENABLE = yes           # Enable N-Key Rollover
-OLED_DRIVER = SSD1306
-OLED_ENABLE = yes
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+# MOUSEKEY_ENABLE = yes        # Mouse keys
+# COMMAND_ENABLE = yes         # Commands for debug and configuration
+# CONSOLE_ENABLE = no         # Console for debug
+# EXTRAKEY_ENABLE = yes        # Audio control and System control
+NKRO_ENABLE = no           # Enable N-Key Rollover
+# OLED_DRIVER = SSD1306
+OLED_ENABLE = no
+RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
