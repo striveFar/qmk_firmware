f117e9d925a35d55607d3018665c595ac4de34df
fafa <1254333071@qq.com>
2022-06-22 20:12:28 +0800

    add nRF5_SDK_17.1.0_ddde560

---
+++ nRF5_SDK_17.1.0_ddde560/examples/peripheral/pwm_driver/pca10040/blank/armgcc/_build/nrf52832_xxaa/system_nrf52.c.d
@@ -0,0 +1,86 @@
+_build/nrf52832_xxaa/system_nrf52.c.o: \
+ ../../../../../../modules/nrfx/mdk/system_nrf52.c \
+ /home/fafa/gcc-arm-none-eabi-7-2018-q2-update/lib/gcc/arm-none-eabi/7.3.1/include/stdint.h \
+ /home/fafa/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/stdint.h \
+ /home/fafa/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/machine/_default_types.h \
+ /home/fafa/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/features.h \
+ /home/fafa/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/_newlib_version.h \
+ /home/fafa/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/_intsup.h \
+ /home/fafa/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/_stdint.h \
+ /home/fafa/gcc-arm-none-eabi-7-2018-q2-update/lib/gcc/arm-none-eabi/7.3.1/include/stdbool.h \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/mpu_armv7.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_name_change.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../../modules/nrfx/mdk/nrf_peripherals.h \
+ ../../../../../../modules/nrfx/mdk/nrf52832_peripherals.h \
+ ../../../../../../modules/nrfx/mdk/nrf_erratas.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_erratas.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_erratas.h \
+ ../../../../../../modules/nrfx/mdk/nrf53_erratas.h \
+ ../../../../../../modules/nrfx/mdk/nrf91_erratas.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52_approtect.h
+
+/home/fafa/gcc-arm-none-eabi-7-2018-q2-update/lib/gcc/arm-none-eabi/7.3.1/include/stdint.h:
+
+/home/fafa/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/stdint.h:
+
+/home/fafa/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/machine/_default_types.h:
+
+/home/fafa/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/features.h:
+
+/home/fafa/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/_newlib_version.h:
+
+/home/fafa/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/_intsup.h:
+
+/home/fafa/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/_stdint.h:
+
+/home/fafa/gcc-arm-none-eabi-7-2018-q2-update/lib/gcc/arm-none-eabi/7.3.1/include/stdbool.h:
+
+../../../../../../modules/nrfx/mdk/nrf.h:
+
+../../../../../../modules/nrfx/mdk/nrf52.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_version.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../../components/toolchain/cmsis/include/mpu_armv7.h:
+
+../../../../../../modules/nrfx/mdk/system_nrf52.h:
+
+../../../../../../modules/nrfx/mdk/nrf52_bitfields.h:
+
+../../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h:
+
+../../../../../../modules/nrfx/mdk/nrf52_name_change.h:
+
+../../../../../../modules/nrfx/mdk/compiler_abstraction.h:
+
+../../../../../../modules/nrfx/mdk/nrf_peripherals.h:
+
+../../../../../../modules/nrfx/mdk/nrf52832_peripherals.h:
+
+../../../../../../modules/nrfx/mdk/nrf_erratas.h:
+
+../../../../../../modules/nrfx/mdk/nrf51_erratas.h:
+
+../../../../../../modules/nrfx/mdk/nrf52_erratas.h:
+
+../../../../../../modules/nrfx/mdk/nrf53_erratas.h:
+
+../../../../../../modules/nrfx/mdk/nrf91_erratas.h:
+
+../../../../../../modules/nrfx/mdk/system_nrf52_approtect.h:
